9e2c41f59f43f94f8d741a34862021f86ee67820
Giuseppe Martone <46596039+giusmar@users.noreply.github.com>
2022-02-01 15:55:52 +0100

    add packages

--- R/create_cm.R
+++ R/create_cm.R
@@ -12,21 +12,26 @@ create_cm <- function(x, ref) {
   rmv <- function(x) gsub(".count.out", "", x)
 
   read_count <- function(x) {
-    read.delim(paste0(here::here("data", "countTable/"), x), comment.char = "#") |>
-      select(Geneid, matches("bam$")) |>
-      rename_at(1, ~"gene_id") |>
-      rename_at(2, ~rmv(x))
+    utils::read.delim(
+      base::paste0(
+        here::here("data", "countTable/"), x),
+      comment.char = "#"
+      ) |>
+      dplyr::select(Geneid, matches("bam$")) |>
+      dplyr::rename_at(1, ~"gene_id") |>
+      dplyr::rename_at(2, ~rmv(x))
   }
 
   temp = x |>
-    map(read_count)
+    purrr::map(read_count)
 
   temp |>
-    Reduce(f = function(x,y) merge(x = x,
+    Reduce(f = function(x,y) base::merge(x = x,
                                    y = y,
-                                   by = intersect(names(x),
-                                                  names(y)))) |>
-    merge(y = ref, by = 'gene_id' ) |>
-    relocate(gene_id, gene_name)
+                                   by = base::intersect(base::names(x),
+                                                  base::names(y)))) |>
+    base::merge(y = ref,
+                by = 'gene_id' ) |>
+    dplyr::relocate(gene_id, gene_name)
 
 }
